78fbc43675187f982323768634ece7975da30354
SHIVA MARISWAMY SUBRAMANI <shivamar@buffalo.edu>
2015-02-14 18:58:09 -0500

    datum

--- edu/buffalo/cse562/Datum.java
+++ edu/buffalo/cse562/Datum.java
@@ -5,7 +5,6 @@ package edu.buffalo.cse562;
 
 import java.util.Date;
 
-<<<<<<< HEAD
 
 public class Datum {
 
@@ -79,63 +78,4 @@ public class Datum {
 //	{
 //		return 0;
 //	}
-=======
-/**
- * @author Sathish
- *
- */
-public class Datum {
-	
-	
-	/**
-	 * 
-	 * @param numberOfColumns
-	 */
-	public Datum(int numberOfColumns)
-	{
-		
-	}
-	
-	/**
-	 * 
-	 * @param input
-	 * @return
-	 */
-	public long toLong(String input)
-	{
-		return 0;
-	}
-	
-	/** 
-	 * 
-	 * @param input
-	 * @return
-	 */
-	public Date toDate(String input)
-	{
-		return null;
-	}
-	
-	/**
-	 * 
-	 * @param input
-	 * @return
-	 */
-	public boolean toBoolean(String input)
-	{
-		return  false;
-	}
-	
-	/**
-	 * 
-	 * @param input
-	 * @return
-	 */
-	
-	public long toInt(String input)
-	{
-		return 0;
-	}
->>>>>>> refs/remotes/origin/master
-
 }
