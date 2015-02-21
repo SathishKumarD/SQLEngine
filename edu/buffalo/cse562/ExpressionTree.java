7aaede2b2aaaabe7744b9ee3e256fa376f718cc7
Oghenekeno <keno.ob@live.com>
2015-02-21 02:45:58 +0000

    Subjoins

--- edu/buffalo/cse562/ExpressionTree.java
+++ edu/buffalo/cse562/ExpressionTree.java
@@ -4,45 +4,26 @@ import java.util.List;
 
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Table;
-import net.sf.jsqlparser.statement.select.AllColumns;
-import net.sf.jsqlparser.statement.select.AllTableColumns;
 import net.sf.jsqlparser.statement.select.FromItem;
 import net.sf.jsqlparser.statement.select.Join;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.SelectBody;
-import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 import net.sf.jsqlparser.statement.select.SelectItem;
 import net.sf.jsqlparser.statement.select.SubJoin;
 import net.sf.jsqlparser.statement.select.SubSelect;
-import net.sf.jsqlparser.statement.select.Union;
  
 public class ExpressionTree {
 	public Operator generateTree(SelectBody sel){
 		Operator current = null;	
 		PlainSelect select = (PlainSelect) sel;
-		FromItem fi = select.getFromItem();
-		Table table  = null;
-		if (fi instanceof Table){
-			table = (Table) fi;
-			String tableName = (table).getWholeTableName();
-			current = new ScanOperator(tableName);			
-		}
-		else if (fi instanceof SubSelect){
-			ExpressionTree exp = new ExpressionTree();
-			current = exp.generateTree(((SubSelect) fi).getSelectBody());
-		}		
-		else if (fi instanceof SubJoin){
-
-		}
-		
+		FromItem fi = select.getFromItem();		
+		current = buildFroms(fi);
+				
 		List<Join> joins = (List<Join>) select.getJoins();
 		if (joins != null){
 			if (joins.size() > 0){
 				for (Join j : joins){
-					FromItem fr = j.getRightItem();
-					if (fr instanceof Table){
-						current = new JoinOperator(current, new ScanOperator(((Table) fr).getName()), j.getOnExpression());
-					}
+					current = buildJoins(current, j);
 				}
 			}
 		}		
@@ -53,26 +34,36 @@ public class ExpressionTree {
 		}
 		
 		List<SelectItem> selItems = (List<SelectItem>) select.getSelectItems();
-//		if (selItems != null){
-//			if (selItems.size() > 0){
-//				for (SelectItem s : selItems){
-//					if (s instanceof AllColumns){
-//						//do nothing, I guess
-//					}
-//					else if (s instanceof AllTableColumns){
-//						//do something
-//					}
-//					else if (s instanceof SelectExpressionItem){
-//						current = new ExtendedProjection(current, (SelectExpressionItem) s);
-//					}
-//				}
-//			}
-//		}
 		if (selItems != null){
 			if (selItems.size() > 0){				
-						current = new ExtendedProjection(current, selItems);
+				current = new ExtendedProjection(current, selItems);
 			}
 		}
+		return current;
+	}
+	public Operator buildFroms(FromItem fi){
+		Table table  = null;
+		Operator current = null;	
+		if (fi instanceof Table){
+			table = (Table) fi;
+			String tableName = (table).getWholeTableName();
+			current = new ScanOperator(tableName);			
+		}
+		else if (fi instanceof SubSelect){
+			current = generateTree(((SubSelect) fi).getSelectBody());
+		}		
+		else if (fi instanceof SubJoin){
+			SubJoin sj = (SubJoin) fi;
+			System.out.println(sj);	
+//			current = buildJoins(buildFroms(sj.getLeft()), sj.getJoin());
+		}
+		return current;
+	}
+	public Operator buildJoins(Operator current, Join j){
+		FromItem fr = j.getRightItem();		
+		if (fr instanceof Table){
+			current = new JoinOperator(current, new ScanOperator(((Table) fr).getName()), j.getOnExpression());
+		}	
 		return current;
 	}
 }
