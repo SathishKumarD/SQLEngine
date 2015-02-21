6c52a053704210b9e07b21e6b38b6ca773719bd5
Sathish Kumar <sdeivasi@buffalo.edu>
2015-02-20 22:35:23 -0500

    fix for column without table name in selection

--- edu/buffalo/cse562/Evaluator.java
+++ edu/buffalo/cse562/Evaluator.java
@@ -6,6 +6,7 @@ package edu.buffalo.cse562;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.LeafValue;
@@ -15,22 +16,48 @@ import edu.buffalo.cse562.Eval;
 public class Evaluator extends Eval {	
 	ArrayList<Tuple> tuple;
 	HashMap<String, ColumnDetail> tupleSchema;
-	
+
 	public Evaluator(ArrayList<Tuple> tuple, HashMap<String, ColumnDetail> tupleSchema)
 	{
 		this.tuple = tuple;
 		this.tupleSchema = tupleSchema;
 	}
-	
+
 	@Override
 	public LeafValue eval(Expression e) throws SQLException{
 		return super.eval(e);
 	}
-	
+
 	@Override
 	public LeafValue eval(Column column) {
-		 int colID = (Integer) tupleSchema.get(column.getWholeColumnName()).getIndex();		  
-		  return tuple.get(colID).val;
+
+		ColumnDetail col = tupleSchema.get(column.getWholeColumnName());
+
+		if(col!=null)
+		{
+			int colID = (Integer) col.getIndex();		  
+			return tuple.get(colID).val;
+		}
+		else
+		{
+			// column may present without table prefix
+			for(Map.Entry<String, ColumnDetail> colDetail: this.tupleSchema.entrySet()){
+				String key = colDetail.getKey();
+				if(key.split("\\.").length>1)
+				{
+					// extract ColumnName from TableName.ColumnName
+					String columnValue = key.split("\\.")[1];
+					
+					// validate it with the column passed
+					if( columnValue.equalsIgnoreCase(column.getWholeColumnName()))
+					{
+						int colID = (Integer) colDetail.getValue().getIndex();		  
+						return tuple.get(colID).val;
+					}
+				}
+			}
+			return null;
+		}
 	}
 
 }
