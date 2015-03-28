a62ea3f44af831dadea8dbceb80940de113d0e73
Sathish Kumar <sdeivasi@buffalo.edu>
2015-03-28 00:18:18 -0400

    commented out sysouts

--- edu/buffalo/cse562/HybridJoinOperator.java
+++ edu/buffalo/cse562/HybridJoinOperator.java
@@ -44,7 +44,7 @@ public class HybridJoinOperator extends JoinOperator{
 				rightTuple = right.readOneTuple();
 			}
 			hashed = true;
-			System.out.println("==== Hashed in " + ((float) (new Date().getTime() - start)/ 1000) + "s");
+			//System.out.println("==== Hashed in " + ((float) (new Date().getTime() - start)/ 1000) + "s");
 		}
 		
 		//try to match more, if the current list is empty
